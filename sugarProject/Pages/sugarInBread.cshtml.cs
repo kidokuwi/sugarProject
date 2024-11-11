f8b79ea6513d0e813926bd61b05f4b735a48e02a
idok <1idokeysar@gmail.com>
2024-11-11 14:39:24 +0200

    uhhh i added to razor pages things

---
+++ sugarProject/Pages/sugarInBread.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace sugarProject.Pages
+{
+    public class sugarInBreadModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
