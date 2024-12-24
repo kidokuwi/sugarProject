e0551a80198c9f4faea889828be094fa33b53481
idok <1idokeysar@gmail.com>
2024-12-24 21:37:49 +0200

    5

--- sugarProject/DataModel/Utils.cs
+++ sugarProject/DataModel/Utils.cs
@@ -3,7 +3,7 @@
     public class Utils
     {
         public const string DB_FILE = "usersDB.mdf";
-        public const string DB_USERS_TABLE = "UsersTbl";
+        public const string DB_USERS_TABLE = "users";
         public const string CONFIG_DB_FILE = "sugarProjectDB";
     }
 }
--- sugarProject/Pages/ShowUsers.cshtml.cs
+++ sugarProject/Pages/ShowUsers.cshtml.cs
@@ -1,12 +1,22 @@
+using sugarProject.DataModel;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-
+using System.Data;
+using ClassicCarsRazor.DataModel;
 namespace sugarProject.Pages
 {
     public class ShowUsersModel : PageModel
     {
-        public void OnGet()
+		public DataTable? DataTableUsers { get; set; }
+        public string[] displayColumns { get; set; } = ["Id", "uName", "fName", "eMail", "yearBorn", "prefix", "phone", "pass"];
+		public IActionResult OnGet()
         {
-        }
+			DBHelper db = new DBHelper();
+			string sqlQuery = $"SELECT * FROM {Utils.DB_USERS_TABLE}";
+			DataTableUsers = db.RetrieveTable(sqlQuery, "users");
+
+			return Page();
+
+		}
     }
 }
