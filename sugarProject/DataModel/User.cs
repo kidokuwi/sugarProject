635c19de2223b8a7485fa143c4b8d69ad241b840
ido <handasvs@gmail.com>
2025-01-27 09:41:38 +0200

    added register

---
+++ sugarProject/DataModel/User.cs
@@ -0,0 +1,20 @@
+﻿namespace sugarProject.DataModel
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string uName { get; set; }
+        public string lName { get; set; }
+        public string fName { get; set; }
+        public string eMail { get; set; }
+        public string gender { get; set; }
+        public int yearBorn { get; set; }
+        public string prefix { get; set; }
+        public string phone { get; set; }
+        public bool eatsWhiteBread {  get; set; }
+        public bool eatsSugarRegularly { get; set; }
+        public bool doesSports {  get; set; }
+        public bool likesBrownBread { get; set; }
+        public string pass {  get; set; }
+    }
+}
---
+++ sugarProject/Pages/Register.cshtml.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using sugarProject.DataModel;
+
+namespace sugarProject.Pages
+{
+    public class RegisterModel : PageModel
+    {
+        [BindProperty]
+        public User user { get; set; }
+        public string st {  get; set; }
+        public void OnGet()
+        {
+        }
+        public void OnPost()
+        {
+            st = "<table>";
+            st += "<tr><td colspan='2' style='text-align:center'>Form Data</td></tr>";
+            st += $"<tr><td>User Id</td> <td>{user.Id}</td></tr>";
+            st += $"<tr><td>User Email</td> <td>{user.eMail}</td></tr>";
+            st += $"<tr><td>User fName</td> <td>{user.fName}</td></tr>";
+            st += $"<tr><td>User lName</td> <td>{user.lName}</td></tr>";
+            st += $"<tr><td>User uName</td> <td>{user.uName}</td></tr>";
+            st += $"<tr><td>User pass</td> <td>{user.pass}</td></tr>";
+            st += $"<tr><td>User year born</td> <td>{user.yearBorn}</td></tr>";
+            st += $"<tr><td>User does sports</td> <td>{user.doesSports}</td></tr>";
+            st += $"<tr><td>User eatsWhiteBread</td> <td>{user.eatsWhiteBread}</td></tr>";
+            st += $"<tr><td>User eats sugar</td> <td>{user.eatsSugarRegularly}</td></tr>";
+            st += $"<tr><td>User prefix</td> <td>{user.prefix}</td></tr>";
+            st += $"<tr><td>User phoe</td> <td>{user.phone}</td></tr>";
+            st += "</table>";
+        }
+        
+        
+    }
+}
