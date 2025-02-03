0f620b0759aff5e58f8da8aff7d5be8e96399b6b
Ido hamelech <1idokeysar@gmail.com>
2025-02-03 10:03:15 +0200

    c

--- sugarProject/wwwroot/js/errorValidate.js
+++ sugarProject/wwwroot/js/errorValidate.js
@@ -1,59 +1,78 @@
-﻿function checkEmail() {
+﻿
+function checkEmail() {
+    let  error = "";
+    let errorEmail = document.getElementById("reg_errormail");
     const emailElement = document.getElementById("reg_email");
     const email = emailElement.value;
-    let errorEmail = document.getElementById("reg_errormail");
-    if (checkAtSymbol(email) && checkAtSymbol(email) && checkLen(email) && validateBadChars(email) && validateHebrew(email)) {
-        errorEmail.innerText = "";
+    if (checkAtSymbol(email) && checkDot(email) && checkLen(email) && validateBadChars(email) && validateHebrew(email)) {
+        errorEmail.innerHTML = "";
     }
-    else {
-        errorEmail.innerText = "bad email";
-    }
-    return (checkAtSymbol(email) && checkAtSymbol(email) && checkLen(email) && validateBadChars(email) && validateHebrew(email))
+    return (checkAtSymbol(email) && checkDot(email) && checkLen(email) && validateBadChars(email) && validateHebrew(email))
 
 }
 
 
 function checkLen(str1) {
+    let errorEmail = document.getElementById("reg_errormail");
+
     if (str1.length > 6) {
         return true;
     }
+    errorEmail.innerHTML = "length";
     return false;
+
 }
 function checkAtSymbol(str1) {
-    if ((str1.indexOf("@") == str1.lastIndexOf("@")) && str1.indexOf("@") != -1 && str1.indexOf("@") != 0 && str1.indexOf("@") != str1.length()-1) {
+    let errorEmail = document.getElementById("reg_errormail");
+
+    if ((str1.indexOf("@") == str1.lastIndexOf("@")) && str1.indexOf("@") != -1 && str1.indexOf("@") != 0 && str1.indexOf("@") != str1.length) {
         return true;
     }
+    errorEmail.innerHTML = "At sign @";
     return false;
+
 }
 function checkDot(str1) {
-    if (str1.indexOf(".") > str1.indexOf("@") + 1) {
-        if (str1.indexOf(".") != str1.length() - 1) {
+    let errorEmail = document.getElementById("reg_errormail");
+
+    if (str1.lastIndexOf(".") > str1.indexOf("@") + 1) {
+        if (str1.indexOf(".") != str1.length - 1) {
             return true;
 
         }
 
     }
+    errorEmail.innerHTML = "dot";
+    return false;
 
 }
 
 function validateHebrew(str1) {
-    let l = str1.length();
+    let errorEmail = document.getElementById("reg_errormail");
+
+    let l = str1.length;
     for (let i = 0; i < l; i++) {
         if (str1.charAt(i) >= 'א' && str1.charAt(i) <= 'ת') {
+            errorEmail.innerHTML = "no hebrew";
             return false;
+
         }
     }
     return true;
 }
 
 function validateBadChars(str1) {
-    let badCh = "!#$%^&*()- <> []{}?+=~"
-    let l = badCh.length();
+    let errorEmail = document.getElementById("reg_errormail");
+
+    let badCh = "!#$%^&*()-<>[]{}?+=~"
+    let l = badCh.length;
 
     for (let i = 0; i < l; i++) {
-        for (let j = 0; j < str1.length(); i++) {
+        for (let j = 0; j < str1.length; i++) {
             if (str1.charAt(j) == badCh.charAt(i)) {
+                errorEmail.innerHTML = "bad chars";
                 return false;
+
             }
         }
     }
