13ed3816b4e3e52ddb9db5bb496e8c82dd7c2f9d
idok <1idokeysar@gmail.com>
2025-03-08 17:55:58 +0200

    Login Form and Time Date - complete

---
+++ sugarProject/Pages/Login.cshtml.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using sugarProject.DataModel;
+using ClassicCarsRazor.DataModel;
+using System.Data;
+
+namespace sugarProject.Pages
+{
+    public class LoginModel : PageModel
+    {
+		public User user { get; set; }
+
+        public string? login_email { get; set; }
+		public string? login_password { get; set; }
+
+        public IActionResult OnPost()
+		{
+			DBHelper db = new DBHelper();
+			DataTable userTable;
+			string sqlQuery = $"SELECT * FROM {Utils.DB_USERS_TABLE} WHERE eMail = '{login_email}' AND pass = '{login_password}'";
+			userTable = db.RetrieveTable(sqlQuery, "users");
+
+			if (userTable.Rows.Count != 1)
+			{
+				return Page();
+			}
+			else
+			{
+				return RedirectToPage("/index");
+			}
+
+			}
+		public void OnGet()
+        {
+        }
+    }
+}
--- sugarProject/wwwroot/js/time.js
+++ sugarProject/wwwroot/js/time.js
@@ -5,13 +5,13 @@ function displayTime() {
     let minutes = date.getMinutes();
     let seconds = date.getSeconds();
     if (hours < 10) {
-        hours += "0" + hours;
+        hours = "0" + hours;
     }
     if (minutes < 10) {
-        minutes += "0" + minutes;
+        minutes = "0" + minutes;
     }
     if (seconds < 10) {
-        seconds += "0" + seconds;
+        seconds = "0" + seconds;
     }
     date = date.getDate() + "/" + (date.getMonth()+1) + "/" + date.getFullYear();
     currentTime += hours + ":" + minutes + ":" + seconds + "  " + date;
