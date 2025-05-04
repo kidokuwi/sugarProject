2367759ed02e89fa3ca925cd07576020b9c55ea8
idok <1idokeysar@gmail.com>
2025-05-04 16:03:50 +0300

    added animations to gallery

--- sugarProject/wwwroot/js/gallery.js
+++ sugarProject/wwwroot/js/gallery.js
@@ -1,6 +1,15 @@
 ﻿function enlarge(imgNum) {
     let img = "b" + imgNum;
     let mainImg = document.getElementById("mainImg");
-    let imgSrc = "/imgs/BreadTypes/"+img+".jpg"
-    mainImg.src = imgSrc;
+    let imgSrc = "/imgs/BreadTypes/" + img + ".jpg";
+
+
+    // rm img
+    mainImg.classList.add("pop-in");
+
+    // after 0.1s change img and appear
+    setTimeout(() => {
+        mainImg.src = imgSrc;
+        mainImg.classList.remove("pop-in");
+    }, 100); 
 }
