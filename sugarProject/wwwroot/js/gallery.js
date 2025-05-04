578816b34c3b6bba5590cb8bba1e5b7b27e6806c
idok <1idokeysar@gmail.com>
2025-05-04 15:45:18 +0300

    gallery works

--- sugarProject/wwwroot/js/gallery.js
+++ sugarProject/wwwroot/js/gallery.js
@@ -1,3 +1,6 @@
-﻿function enlarge() {
-
+﻿function enlarge(imgNum) {
+    let img = "b" + imgNum;
+    let mainImg = document.getElementById("mainImg");
+    let imgSrc = "/imgs/BreadTypes/"+img+".jpg"
+    mainImg.src = imgSrc;
 }
